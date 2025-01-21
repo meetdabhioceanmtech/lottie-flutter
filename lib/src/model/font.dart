da4a7da0a82f48c31ff01c839ed579dee0752a7d
meet dabhi oceanmtech <meetdabhi.oceanmtech@gmail.com>
2025-01-21 10:05:46 +0530

    minor changing added

--- lib/src/model/font.dart
+++ lib/src/model/font.dart
@@ -1,10 +1,18 @@
 class Font {
+  final int origion;
+  final String fontPath;
+  final String fontClass;
+  final String fWeight;
   final String family;
   final String name;
   final String style;
   final double ascent;
 
   Font({
+    required this.origion,
+    required this.fontPath,
+    required this.fontClass,
+    required this.fWeight,
     required this.family,
     required this.name,
     required this.style,
